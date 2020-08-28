daeb9447986cc9b1cacc329fd16c57399186ddc4
Rafael Gallo <rafaelhenriquegallo@gmail.com>
2020-08-28 20:40:50 -0300

    Update Web scraping - clima.py

--- Web-scraping/Web scraping - clima.py
+++ Web-scraping/Web scraping - clima.py
@@ -6,7 +6,7 @@ from bs4 import BeautifulSoup
 from datetime import date
 
 date = date.today()
-site_HTML = requests.get("https://api.hgbrasil.com/weather?key=e1a836da&lat=-23.682&log=-46.875&user_ip=remote").content
+site_HTML = requests.get("https://api.hgbrasil.com/weather?key="KEY"=-23.682&log=-46.875&user_ip=remote").content
 
 s = BeautifulSoup(site_HTML, "html.parser")
 day = json.loads(s.prettify())
